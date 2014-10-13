8948ad4293b619d87f6bf6b301916a6f0190a623
dbogle <dbogle22@byu.edu>
2014-10-13 15:24:46 -0600

    boss?

--- hello_world_0/src/globals.h
+++ hello_world_0/src/globals.h
@@ -1,4 +1,5 @@
-#define ALIEN_HEIGHT 16
+#define ALIEN_HEIGHT 16;
+#define BOSS_HEIGHT 14;
 #define ALIEN_BLOCK_HEIGHT 120;	//total alien height
 #define ALIEN_BLOCK_WIDTH 352;		//total un-altered alien width
 
